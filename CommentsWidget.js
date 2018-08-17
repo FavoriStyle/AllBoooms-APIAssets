7290da6beb71710f6d08845f426eb79cdbfd4848
KaMeHb-UA <marlock@etlgr.com>
2018-08-17 09:49:52 +0300

    Comments list updated (now must works)

--- CommentsWidget.js
+++ CommentsWidget.js
@@ -16,7 +16,7 @@ module.exports = class CommentsWidget{
         options = Object.assign({
             lang: 'ru'
         }, options);
-        const dictionary = Object.assign(dictionary[options.lang], options.strings);
+        const dict = Object.assign(dictionary[options.lang], options.strings);
         // Создаём контейнер
         this.conainer = document.createElement('div');
         this.conainer.id = `comments-widget-${widgetID}`;
@@ -30,37 +30,58 @@ module.exports = class CommentsWidget{
         this.conainer.appendChild(submit);
         this.conainer.appendChild(commentsList);
         // Применяем настройки
-        myComment.placeholder = dictionary.placeholder;
+        myComment.placeholder = dict.placeholder;
         myComment.setAttribute('class', 'comment-input');
         submit.type = 'submit';
         submit.setAttribute('class', 'comment-input-submit');
-        submit.value = dictionary.submitText;
+        submit.value = dict.submitText;
         // Изменяем поведение кнопки
         submit.addEventListener('click', async ev => {
             ev.preventDefault();
             if(myComment.value){
-                submit.value = dictionary.submittingText;
+                submit.value = dict.submittingText;
                 await API.comments.external.add({
                     token: "`3\\M`Yo'&9I+'sN&W%G\"ldxGAtE+LJMyc.8ngRY!j=i!'|Q35^'\"wW*5g)D\"WfC@",
                     widget_id: widgetID,
                     text: myComment.value,
                 });
-                submit.value = dictionary.submitText;
+                submit.value = dict.submitText;
             }
         })
         // Периодично запрашиваем новые комментарии
         (() => {
+            function normalizeDate(Date){
+                return 'вчера'
+            }
+            function createCommentElement({id, text, user: {id: uid, fullName: uname, avatar: uavatar}, timestamp}){
+                const userAvatar = document.createElement('img'),
+                    userName = document.createElement('a'),
+                    commentText = document.createElement('p'),
+                    commentTime = document.createElement('span'),
+                    container = document.createElement('div');
+                userAvatar.setAttribute('src', uavatar);
+                userName.setAttribute('href', `${API.baseHost}/${uid}`);
+                userName.innerText = uname;
+                commentText.innerText = text;
+                commentTime.innerText = normalizeDate(new Date(timestamp * 1000));
+                container.appendChild(userAvatar);
+                container.appendChild(userName);
+                container.appendChild(commentTime);
+                container.appendChild(commentText);
+                return container
+            }
+            function updateContainer(toPrepend){
+                toPrepend.forEach(element => commentsList.insertBefore(createCommentElement(element), commentsList.firstElementChild))
+            }
             const comments = new class extends Array{
                 prependNew(tagretArray){
                     var toPrepend = [];
                     for(var i = 0; i < tagretArray.length; i++){
                         if(tagretArray[i].id != this[0].id) toPrepend.push(tagretArray[i]); else {
+                            updateContainer(toPrepend);
                             return this.unshift(...toPrepend)
                         }
                     }
-                }
-                updateContainer(){
-                    //commentsList
                 }
             };
             (async function requestComments(){
