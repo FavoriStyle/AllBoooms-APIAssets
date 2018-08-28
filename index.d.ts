e3529a3f0fe46f29cb6c63e763bece5b7d483af1
KaMeHb-UA <marlock@etlgr.com>
2018-08-28 22:01:46 +0300

    [Refactor] Stage 2. Exporting intermediate types

--- index.d.ts
+++ index.d.ts
@@ -5,140 +5,139 @@ declare const require:function(String):Promise<any>;
 declare const __dirname:String;
 declare const __filename:String;
 declare namespace API{
-    export const Reference = (() => {
-        type Attachments = Array<{
-            type: 'image',
-            src: String,
-        } | {
-            type: 'link',
-            src: String,
-            preview: String,
+    export type Attachments = Array<{
+        type: 'image',
+        src: String,
+    } | {
+        type: 'link',
+        src: String,
+        preview: String,
+        text: String,
+        title: String,
+    } | {
+        type: 'document',
+        src: String,
+        documentType: 'text' | 'office_doc' | 'office_table' | 'office_presentation' | 'not_recognized',
+        name: String,
+    } | {
+        type: 'audio',
+        src: String,
+        artist: String,
+        name: String,
+    } | {
+        type: 'video',
+        src: String,
+        name: String,
+        length: Number,
+        videoType: 'direct' | 'youtube',
+    }>;
+    export type CommentList = {
+        comments: Array<{
+            id: String,
             text: String,
-            title: String,
-        } | {
-            type: 'document',
-            src: String,
-            documentType: 'text' | 'office_doc' | 'office_table' | 'office_presentation' | 'not_recognized',
-            name: String,
-        } | {
-            type: 'audio',
-            src: String,
-            artist: String,
-            name: String,
-        } | {
-            type: 'video',
-            src: String,
-            name: String,
-            length: Number,
-            videoType: 'direct' | 'youtube',
-        }>;
-        interface UserList{
-            [key: String]: {
-                fullName: String,
-                avatar: String,
-            }
-        };
-        return class APIReference{
-            messages: {
-                chats: {
-                    list(args: {
-                        token: String,
-                    }):Promise<Array<String>>
-                },
+            userid: String,
+            timestamp: Number,
+        }>,
+        users: UserList,
+    }
+    export interface UserList{
+        [key: String]: {
+            fullName: String,
+            avatar: String,
+        }
+    };
+    export class Reference{
+        messages: {
+            chats: {
                 list(args: {
                     token: String,
-                    chat_id: String,
-                    count?: Number,
-                    after?: String,
-                    reversed?: Boolean,
-                }):Promise<Array<{
+                }):Promise<Array<String>>
+            },
+            list(args: {
+                token: String,
+                chat_id: String,
+                count?: Number,
+                after?: String,
+                reversed?: Boolean,
+            }):Promise<Array<{
+                text: String,
+                timestamp: Number,
+                edited: Number,
+                id: String,
+                sender: String,
+                attachments?: Attachments,
+            }>>,
+            send(args: {
+                token: String,
+                destination: String,
+                message: {
                     text: String,
-                    timestamp: Number,
-                    edited: Number,
-                    id: String,
-                    sender: String,
                     attachments?: Attachments,
-                }>>,
-                send(args: {
-                    token: String,
-                    destination: String,
-                    message: {
-                        text: String,
-                        attachments?: Attachments,
-                    },
-                }):Promise<{
-                    timestamp: Number,
+                },
+            }):Promise<{
+                timestamp: Number,
+                id: String,
+            }>,
+            edit(args: {
+                token: String,
+                destination: String,
+                message: {
                     id: String,
-                }>,
-                edit(args: {
+                    text: String,
+                    attachments?: Attachments,
+                },
+            }):Promise<Number>,
+            delete(args: {
+                token: String,
+                destination: String,
+                message_id: String,
+                forAnyone?: Boolean,
+            }):Promise<true>,
+        };
+        comments: {
+            external: {
+                list(args: {
+                    app_id: String,
+                    widget_id: String,
+                }):Promise<CommentList>,
+                add(args: {
                     token: String,
-                    destination: String,
-                    message: {
-                        id: String,
-                        text: String,
-                        attachments?: Attachments,
-                    },
-                }):Promise<Number>,
-                delete(args: {
-                    token: String,
-                    destination: String,
-                    message_id: String,
-                    forAnyone?: Boolean,
-                }):Promise<true>,
-            };
-            comments: {
-                external: {
-                    list(args: {
-                        app_id: String,
-                        widget_id: String,
-                    }):Promise<{
-                        comments: Array<{
-                            id: String,
-                            text: String,
-                            userid: String,
-                            timestamp: Number,
-                        }>,
-                        users: UserList,
-                    }>,
-                    add(args: {
-                        token: String,
-                        widget_id: String,
-                        text: String,
-                    }):Promise<{
-                        id: String,
-                        timestamp: Number,
-                    }>,
-                }
-            };
-            user: {
-                getInfo(args: {
-                    token: String,
-                    list: Array<
-                        'avatar'
-                      | 'email'
-                      | 'fullName'
-                      | 'id'
-                      | 'tagLine'
-                      | 'banner'
-                      | 'birthday'
-                      | 'companyName'
-                      | 'twitterId'
-                      | 'webUrl'
-                    >,
+                    widget_id: String,
+                    text: String,
                 }):Promise<{
-                    avatar?: String,
-                    email?: String,
-                    fullName?: String,
-                    id?: String,
-                    tagLine?: String,
-                    banner?: String,
-                    birthday?: Number,
-                    companyName?: String,
-                    twitterId?: String,
-                    webUrl?: String,
+                    id: String,
+                    timestamp: Number,
                 }>,
             }
-            static baseHost:String;
+        };
+        user: {
+            getInfo(args: {
+                token: String,
+                list: Array<
+                    'avatar'
+                  | 'email'
+                  | 'fullName'
+                  | 'id'
+                  | 'tagLine'
+                  | 'banner'
+                  | 'birthday'
+                  | 'companyName'
+                  | 'twitterId'
+                  | 'webUrl'
+                >,
+            }):Promise<{
+                avatar?: String,
+                email?: String,
+                fullName?: String,
+                id?: String,
+                tagLine?: String,
+                banner?: String,
+                birthday?: Number,
+                companyName?: String,
+                twitterId?: String,
+                webUrl?: String,
+            }>,
         }
-    })();
+        static baseHost:String;
+    }
 }
