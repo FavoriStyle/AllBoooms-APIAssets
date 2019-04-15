a534793aeb3f1489acb2c5e7b2089ab174e3d2f7
KaMeHb-UA <marlock@etlgr.com>
2019-04-15 17:57:55 +0300

    New build system

---
+++ build.mjs
@@ -0,0 +1,63 @@
+import {
+    readFileSync as readFile,
+    writeFileSync as writeFile,
+    readdirSync as readdir,
+    mkdirSync,
+} from 'fs'
+import uglify from 'uglify-es'
+import path from 'path'
+import rimraf from 'rimraf'
+
+const currentFile = decodeURI(import.meta.url).replace(/^file:\/+/, '/');
+const srcDir = path.resolve(currentFile, '..', 'src');
+const distDir = path.resolve(srcDir, '..', 'dist');
+
+function mkdir(dir){
+    try{
+        mkdirSync(dir, { recursive: true })
+    } catch(e){
+        if(e.code !== 'EEXIST') throw e
+    }
+}
+
+rimraf.sync(distDir);
+mkdir(distDir);
+
+function directoryLister(dir){
+    readdir(dir, {
+        encoding: 'utf8',
+        withFileTypes: true,
+    }).forEach(dirent => {
+        const direntPath = path.resolve(dir, dirent.name);
+        if(dirent.isDirectory()) directoryLister(direntPath);
+        else if(dirent.isFile() && path.extname(direntPath) === '.js'){
+            const filePath = path.relative(srcDir, direntPath);
+            process.stdout.write(`Minifying ${filePath}... `);
+            const minified = uglify.minify(readFile(direntPath, 'utf8'));
+            if(!minified.code) throw new Error(minified.error);
+            const destination = path.resolve(distDir, filePath);
+            mkdir(path.resolve(destination, '..'));
+            writeFile(destination, minified.code, 'utf8');
+            console.log('Ok')
+        }
+    })
+}
+
+directoryLister(srcDir);
+
+/*
+readdir()
+
+const options = {};
+
+flist.forEach(fname => {
+    process.stdout.write(`Building ${fname}.js...`);
+    var minified = uglify.minify(fs.readFileSync(fname + '.js', 'utf8'), options);
+    if(!minified.code){
+        console.error('uglify.minify(...).code is not defined properly\n');
+        throw new TypeError(minified.error)
+    }
+    fs.writeFileSync(fname + '.js', minified.code, 'utf8');
+    console.log(`Ok`);
+});
+*/
