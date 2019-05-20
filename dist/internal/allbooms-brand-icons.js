b584f3cd1ccd83435567998022fae687f5d90b38
KaMeHb-UA <marlock@etlgr.com>
2019-05-20 17:18:08 +0300

    Updated version of icons

--- dist/internal/allbooms-brand-icons.js
+++ dist/internal/allbooms-brand-icons.js
@@ -1,1 +1,1 @@
-import loadFont from"./font-loader.js";export const fontName="AllBooms Brand Icons";export const cssURI="https://cdn.jsdelivr.net/gh/FavoriStyle/AllBooms-brand-icons@3/dist/allbooms-brand-icons.css";loadFont(fontName,cssURI);
+import loadFont from"./font-loader.js";export const fontName="AllBooms Brand Icons";export const cssURI="https://cdn.jsdelivr.net/gh/FavoriStyle/AllBooms-brand-icons@4/dist/css/allbooms-brand-icons.min.css";loadFont(fontName,cssURI);
--- src/internal/allbooms-brand-icons.js
+++ src/internal/allbooms-brand-icons.js
@@ -1,6 +1,6 @@
 import loadFont from './font-loader.js'
 
 export const fontName = 'AllBooms Brand Icons'
-export const cssURI = 'https://cdn.jsdelivr.net/gh/FavoriStyle/AllBooms-brand-icons@3/dist/allbooms-brand-icons.css'
+export const cssURI = 'https://cdn.jsdelivr.net/gh/FavoriStyle/AllBooms-brand-icons@4/dist/css/allbooms-brand-icons.min.css'
 
 loadFont(fontName, cssURI)
